49fc7d3493b1bdf9206881c213b7db7babd9f58c
tarunpatelTMI <tarun.patel@trafficmanagement.com>
2023-02-28 11:47:15 +0530

    Initial setup

--- App.tsx
+++ App.tsx
@@ -1,9 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { QueryClient, useQuery } from '@tanstack/react-query'
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client'
+import { createAsyncStoragePersister } from '@tanstack/query-async-storage-persister'
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      cacheTime: 1000 * 60 * 60 * 24, // 24 hours
+    },
+  },
+})
+const asyncStoragePersister = createAsyncStoragePersister({
+  storage: AsyncStorage
+})
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -13,10 +28,14 @@ export default function App() {
     return null;
   } else {
     return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
+      <PersistQueryClientProvider
+        client={queryClient}
+        persistOptions={{ persister: asyncStoragePersister }}>
+        <SafeAreaProvider>
+          <Navigation colorScheme={colorScheme} />
+          <StatusBar />
+        </SafeAreaProvider>
+      </PersistQueryClientProvider>
     );
   }
 }
